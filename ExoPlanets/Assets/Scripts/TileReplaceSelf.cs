b2e5b4835465df705b671638314a4746b9434ea8
thefil <filipkrynicki@gmail.com>
2014-01-18 23:33:33 -0500

    dynamic tiles that load themselves. Should work even in another prefab

---
+++ ExoPlanets/Assets/Scripts/TileReplaceSelf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileReplaceSelf : MonoBehaviour {
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject parent = this.transform.parent.gameObject;
+		GameObject replacement = Instantiate (prefab, transform.position, transform.rotation) as GameObject;
+		replacement.transform.parent = parent.transform;
+		Destroy (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
