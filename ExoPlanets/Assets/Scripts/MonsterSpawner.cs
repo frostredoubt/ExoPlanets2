d4ca3ab45ec5957530c5de03981c9ccabba8e826
thefil <filipkrynicki@gmail.com>
2014-01-19 17:55:07 -0500

    spawning enemies in randomly generated level

---
+++ ExoPlanets/Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterSpawner : MonoBehaviour {
+
+	public GameObject[] monsters;
+	public float chance;
+
+	// Use this for initialization
+	void Awake () {
+		if (Random.value < chance) {
+			Transform parentTransform = this.transform.parent;
+
+			GameObject prefab = monsters[Random.Range(0, monsters.Length)];
+
+			GameObject replacement = Instantiate (prefab, this.transform.position, this.transform.rotation) as GameObject;        
+			replacement.transform.parent = parentTransform;
+		}
+		Destroy (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
