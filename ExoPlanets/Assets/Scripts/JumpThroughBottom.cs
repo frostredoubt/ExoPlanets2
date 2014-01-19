4316de450b241f9d7e95c0892e0b11ffa8f41cba
thefil <filipkrynicki@gmail.com>
2014-01-19 12:18:45 -0500

    forgot the script

---
+++ ExoPlanets/Assets/Scripts/JumpThroughBottom.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpThroughBottom : MonoBehaviour {
+
+	private Collider2D block;
+
+	private bool through = false;
+
+	// Use this for initialization
+	void Start () {
+		block = this.transform.parent.gameObject.collider2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		block.enabled = !through;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+
+			through = true;
+		}
+			
+		
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player")
+						through = false;
+		
+	}
+}
