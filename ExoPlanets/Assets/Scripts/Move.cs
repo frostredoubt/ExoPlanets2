03a71c7b1c4866a8527012d89f3a4975900fe136
Alex Szlavik <aszlavik@uwaterloo.ca>
2014-01-18 21:29:47 -0500

    Change of character direction

--- ExoPlanets/Assets/Scripts/Move.cs
+++ ExoPlanets/Assets/Scripts/Move.cs
@@ -10,22 +10,37 @@ public class Move : MonoBehaviour {
 
     private Vector2 Forces;
     private bool Jump_pressed;
+    private Vector3 Current_direction;
 
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         Gather_inputs();
-        Correct_speed();
+        Change_displayed_components();
         Apply_forces();
 
         Debug_output();
 	}
 
-    void Correct_speed()
+    private void Change_displayed_components()
     {
+        if (Current_direction.x < 0 && Forces.x > 0)
+            Change_character_direction();
+        if (Current_direction.x > 0 && Forces.x < 0)
+            Change_character_direction();
+        if( Forces.x != 0 )
+            Current_direction = Forces.normalized;
+    }
+
+    void Change_character_direction()
+    {
+        Vector3 old = transform.localScale;
+        old.x = -old.x;
+        transform.localScale = old;
     }
 
     void Gather_inputs()
@@ -44,15 +59,18 @@ public class Move : MonoBehaviour {
 
         old_v_x_mag.Set(old_x, 0);
 
-        if(old_v_x_mag.magnitude < Max_x_velocity)
+        if (old_v_x_mag.magnitude < Max_x_velocity)
+        {
             gameObject.rigidbody2D.AddForce(Forces);
-        else if((new Vector2(old_v.x + Forces.x,0)).magnitude < old_v_x_mag.magnitude)
+        }
+        else if ((new Vector2(old_v.x + Forces.x, 0)).magnitude < old_v_x_mag.magnitude)
+        {
             gameObject.rigidbody2D.AddForce(Forces);
+        }
 
         // Jump
         Transform ground = transform.Find("Ground_collider");
         bool hit = Physics2D.Linecast(transform.position, ground.position, 1 << LayerMask.NameToLayer("Ground"));
-        //Debug.DrawLine(transform.position, ground.position);
         if (Jump_pressed && hit)
         {
             gameObject.rigidbody2D.AddForce(new Vector2(0, Jump_Force));
@@ -71,6 +89,6 @@ public class Move : MonoBehaviour {
 
     void Debug_output()
     {
-        //Debug.Log(gameObject.rigidbody2D.velocity);
+       
     }
 }
