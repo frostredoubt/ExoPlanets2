948dd20e71b623fdb168f51ec58cc1827911d9d7
Alex Szlavik <aszlavik@uwaterloo.ca>
2014-01-19 01:22:59 -0500

    basic sword swing. only forward

--- ExoPlanets/Assets/Scripts/Attack.cs
+++ ExoPlanets/Assets/Scripts/Attack.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class Attack : MonoBehaviour {
 
+    private bool Is_fire = false;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        Get_input();
+        Do_attack();
 	}
+
+    void Get_input()
+    {
+        Is_fire = Input.GetButton("Fire1");
+    }
+
+    void Do_attack()
+    {
+        if (Is_fire)
+            GetComponent<Animator>().SetBool("do_swing", true);
+        else
+            GetComponent<Animator>().SetBool("do_swing", false);
+    }
 }
