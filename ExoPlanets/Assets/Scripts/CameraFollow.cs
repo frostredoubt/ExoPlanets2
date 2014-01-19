7133cf4f2c625e429d5a0b53a18d4e715854e033
thefil <filipkrynicki@gmail.com>
2014-01-18 20:20:39 -0500

    basic static level

---
+++ ExoPlanets/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
+	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
+
+	private Transform player;		// Reference to the player's transform.
+	
+	
+	void Awake ()
+	{
+		// Setting up the reference.
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		TrackPlayer();
+	}
+	
+	
+	void TrackPlayer ()
+	{
+		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
+		float targetX = player.position.x;
+		float targetY = player.position.y;
+		
+		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
+		//targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+		//targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+		
+		// Set the camera's position to the target position with the same z component.
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+}
+
