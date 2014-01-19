1d20c7415efee45a5b599ba2e42c7bbac1147652
becassel <becassel@uwaterloo.ca>
2014-01-19 13:58:52 -0500

    Updated a bunch of template and tile replacement scripts. Added the ability to easily insert more feature replacement into level generation. Updated LevelGenerator scene to be in line with Fil's most recent template changes. Next goal is to allow off-required-path generation.

--- ExoPlanets/Assets/Scripts/TileReplaceSelf.cs
+++ ExoPlanets/Assets/Scripts/TileReplaceSelf.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
 
+/// <summary>
+/// Represents that tiles are initially empty prefabs which destroy themselves and replace themsleves with a true prefab.
+/// This allows dynamism and nested prefabs to continue updating whereas they normally would not be able to because Unity
+/// makes some weird decisions about object hierarchy splitting.
+/// </summary>
 public class TileReplaceSelf : MonoBehaviour
 {
 
+    /// <summary>
+    /// The actual prefab to associate with a tile.
+    /// </summary>
 	public GameObject prefab;
 
-	// Use this for initialization
+	/// <summary>
+	/// During awake, destroy this tile and instead instantiate another tile underneath its parent.
+	/// </summary>
 	void Awake()
     {
-		GameObject parent = this.transform.parent.gameObject;
-		GameObject replacement = Instantiate(prefab, transform.position, transform.rotation) as GameObject;        
-		replacement.transform.parent = parent.transform;
+		Transform parentTransform = this.transform.parent;
+		GameObject replacement = Instantiate(prefab, this.transform.position, this.transform.rotation) as GameObject;        
+		replacement.transform.parent = parentTransform;
 		Destroy(this.gameObject);
         return;
 	}
